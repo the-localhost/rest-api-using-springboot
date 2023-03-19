d8cc9807165402d74878eedf4dfff9750761847a
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-03-19 22:50:45 +0530

    added a new method findByRole

--- src/main/java/com/learn/springboot/firstrestapi/user/UserDetailsRepository.java
+++ src/main/java/com/learn/springboot/firstrestapi/user/UserDetailsRepository.java
@@ -1,7 +1,9 @@
 package com.learn.springboot.firstrestapi.user;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
-
+	List<UserDetails> findByRole(String role);
 }
