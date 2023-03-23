9acec3abd5d55837bf8e476267e96a48e49c6c7f
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-03-23 12:13:35 +0530

    used JSONAssert to match the response with expected value

--- src/test/java/com/learn/springboot/firstrestapi/survey/SurveyResourceIT.java
+++ src/test/java/com/learn/springboot/firstrestapi/survey/SurveyResourceIT.java
@@ -1,8 +1,8 @@
 package com.learn.springboot.firstrestapi.survey;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -32,12 +32,14 @@ public class SurveyResourceIT {
 	private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
 	
 	@Test
-	void retrieveSurveyQuestionById_basicScenario(){
+	void retrieveSurveyQuestionById_basicScenario() throws JSONException{
 		ResponseEntity<String> responseEntity = template.getForEntity(SPECIFIC_QUESTION_URL, String.class);
 		String expectedResponse = """
-				{"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"}
+				{"id":"Question1",
+				"description":"Most Popular Cloud Platform Today",
+				"correctAnswer":"AWS"}
 				""";
-		assertEquals(expectedResponse.trim(), responseEntity.getBody());
+		JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
 		System.out.println(responseEntity.getBody());
 		System.out.println(responseEntity.getHeaders());
 	}
