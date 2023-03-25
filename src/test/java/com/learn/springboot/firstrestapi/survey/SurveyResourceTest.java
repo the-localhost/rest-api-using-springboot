6e7377880e2c4288300b89dafd2cc60a621a714a
Gagan Agrawal <gaganagrawal.iit@gmail.com>
2023-03-25 18:17:11 +0530

    Created mock test and stubbed method to return bean

--- src/test/java/com/learn/springboot/firstrestapi/survey/SurveyResourceTest.java
+++ src/test/java/com/learn/springboot/firstrestapi/survey/SurveyResourceTest.java
@@ -1,14 +1,20 @@
 package com.learn.springboot.firstrestapi.survey;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.http.MediaType;
 
 @WebMvcTest(controllers = SurveyResource.class)
 public class SurveyResourceTest {
@@ -23,16 +29,39 @@ public class SurveyResourceTest {
 			"http://localhost:8080/surveys/Survey1/questions/Question1";
 	
 	@Test
-	void retrieveSurveyQuestionById_404basicScenario() throws Exception {
+	void retrieveSurveyQuestionById_404Scenario() throws Exception {
 		RequestBuilder requestBuilder = 
 				MockMvcRequestBuilders.get(SPECIFIC_QUESTION_url).accept(MediaType.APPLICATION_JSON);
 		
 		MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
 		
-		System.out.println(mvcResult.getResponse().getContentAsString());
-		
-		// return 404 as the mock surveyService does not return anything
+		// returns 404 as the mock surveyService does not return anything
 		// and we will need a stub function
-		System.out.println(mvcResult.getResponse().getStatus());
+		assertEquals(404, mvcResult.getResponse().getStatus());
+	}
+	
+	@Test
+	void retrieveSurveyQuestionById_basicScenario() throws Exception {
+		RequestBuilder requestBuilder = 
+				MockMvcRequestBuilders.get(SPECIFIC_QUESTION_url).accept(MediaType.APPLICATION_JSON);
+		
+		Question question = new Question("Question1", "Most Popular Cloud Platform Today",
+				Arrays.asList("AWS", "Azure", "Google Cloud", "Oracle Cloud"), "AWS");
+		
+		String expectedResponse = """
+				{
+					"id":"Question1",
+					"description":"Most Popular Cloud Platform Today",
+					"options":["AWS","Azure","Google Cloud","Oracle Cloud"],
+					"correctAnswer":"AWS"
+				}
+				""";
+		
+		when(surveyService.retrieveSurveyQuestionById("Survey1", "Question1")).thenReturn(question);
+		
+		MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+		
+		JSONAssert.assertEquals(expectedResponse, mvcResult.getResponse().getContentAsString(), false);
+		assertEquals(200, mvcResult.getResponse().getStatus());
 	}
 }
